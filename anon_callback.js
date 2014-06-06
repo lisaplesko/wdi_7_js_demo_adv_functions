3a85ecb22959a398e53a66eb1f3aae5add86c49a
Lisa Plesko <plesko.l@gmail.com>
2014-06-06 15:57:34 -0400

    Removed unnecessary variables

--- anon_callback.js
+++ anon_callback.js
@@ -1,20 +1,18 @@
-// The callback here is an anonymous function defined when 
+// The callback here is an anonymous function defined when
 // this function is called below.
 
-// Don't believe it's any more or less efficient that 
+// Don't believe it's any more or less efficient that
 // explicity defining the callback function.
 
 // Apply the callback to all DOM elements, nodes, that
 // have the class 'test'
 var findNodes = function (callback) {
-	var nodes = [], // stores the result
-	found; // the next node found 
+	var nodes = []; // stores the result
 	// get all the DOM elements
 	var testElements = document.getElementsByClassName("test");
 	for(var i = 0; i < testElements.length; i++){
 			callback(testElements[i]);
-	};
-	return nodes; 
+	}
 };
 
 var msg = "Hide All DOM elements with the class 'test' with an anonymous callback";
@@ -22,7 +20,7 @@ alert(msg);
 console.log(msg);
 
 
-// The callback is defined in-place,i.e. when the function is being using 
+// The callback is defined in-place,i.e. when the function is being using
 // the callback is being invoked.
 findNodes(function hideNode(node){
 	node.style.display = "none";
@@ -35,4 +33,3 @@ findNodes(function hideNode(node){
 // findNodes(function(node){
 // 	node.style.display = "none";
 // });
-
--- problem_callback.js
+++ problem_callback.js
@@ -5,18 +5,17 @@ myapp.color = "green"; // color property is a String
 
 // paint property is a Function
 myapp.paint = function (node) {
-	node.style.color = this.color; 
+	node.style.color = this.color;
 };
 
 
 // Function that will ATTEMPT to make the DOM elements
 // with a class of 'test' green. THIS WILL FAIL!!
 
-// It takes a callback function that is invoked for 
+// It takes a callback function that is invoked for
 // each DOM element of class 'test' and set it's color to green.
 var findNodes = function (callback) {
-	var nodes = [], // stores the result
-	found; // the next node found 
+	var nodes = []; // stores the result
 	// get all the DOM elements
 	var testElements = document.getElementsByClassName("test");
 	for(var i = 0; i < testElements.length; i++){
@@ -24,25 +23,23 @@ var findNodes = function (callback) {
 			// the this pointer is the global object because this
 			// findNodes function is a global object.
 			callback(testElements[i]);
-	};
-	return nodes; 
+	}
 };
 
-// Function that will make the DOM elements with a class of 'test' green. 
+// Function that will make the DOM elements with a class of 'test' green.
 
 // It uses the javascript 'call' function to change the
 // 'Context' that the callback function is invoked from.
 // Changing the Context will change the 'this' pointer used
 // inside the callback function.
 var findNodes_fixed = function (callback,callback_obj) {
-	var nodes = [], // stores the result
-	found; // the next node found 
+	var nodes = []; // stores the result
 	// get all the DOM elements
 	var testElements = document.getElementsByClassName("test");
 	for(var i = 0; i < testElements.length; i++){
 			callback.call(callback_obj, testElements[i]);
-	};
-	return nodes; 
+			// make the myapp.paint call and make sure this pointer is myapp
+	}
 };
 
 // Fails because 'this' pointer is WRONG
@@ -55,4 +52,4 @@ console.log(msg);
 
 // Succeeds because the 'this' pointer is explicity
 // set to the myapp object.
-findNodes_fixed(myapp.paint, myapp)
+findNodes_fixed(myapp.paint, myapp);
